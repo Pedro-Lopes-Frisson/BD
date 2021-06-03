70ab309a7f7d6666828c45f559519a8cb3855c97
Pedro-Lopes-Frisson <61754260+Pedro-Lopes-Frisson@users.noreply.github.com>
2021-06-03 15:07:25 +0100

    asdf

--- trabalho_pratico/tabelas.sql
+++ trabalho_pratico/tabelas.sql
@@ -1,8 +1,31 @@
+USE trabalhoPratico;
+GO
+
+--DROP TABLE consumable;
+--DROP TABLE cosmetic;
+--DROP TABLE specialItem;
+--DROP TABLE vendorStash;
+--DROP TABLE buyerStash;
+--DROP TABLE normalTab;
+--DROP TABLE specialTab;
+--DROP TABLE melee;
+--DROP TABLE shield;
+--DROP TABLE armor;
+--DROP TABLE physical;
+--DROP TABLE magical;
+--DROP TABLE ranged;
+--DROP TABLE weapon;
+--DROP TABLE [stash];
+--DROP TABLE stashTabs;
+--DROP TABLE item;
+--DROP TABLE [user];
+
+
 
 CREATE TABLE specialItem (
 
-            ID  int   --NOT NULL,
-            Name  varchar(128)  NOT NULL,
+            ID  int,   --NOT NULL,
+            [Name]  varchar(128)  NOT NULL,
             Price  smallmoney  NOT NULL,
             CurrencyType  bit CONSTRAINT default_currencyType DEFAULT 1, -- By default special items are paid with real money
             -- 1 for real money
@@ -11,22 +34,22 @@ CREATE TABLE specialItem (
           )
 
 CREATE TABLE consumable (
-            SpecialItem_ID  int   FOREIGN KEY REFERENCES specialItem(ID)-- NOT NULL,
-            Effect          varchar(128) NOT NULL,
-            Duration        DOUBLE(100,3)       NOT NULL,
+            SpecialItem_ID  int   FOREIGN KEY REFERENCES specialItem(ID), -- NOT NULL,
+            [Effect]          varchar(128) NOT NULL,
+            Duration        decimal(38,3)       NOT NULL,
             Quantity        int          ,
-            PRIMARY KEY (SpecialItem_ID)
-            CONSTRAINT FOREIGN KEY (SpecialItem_ID) references [specialItem] (ID)
+            PRIMARY KEY (SpecialItem_ID),
+            FOREIGN KEY (SpecialItem_ID) references [specialItem] (ID)
 
 )
 
 CREATE TABLE cosmetic (
-            SpecialItem_ID  int   FOREIGN KEY REFERENCES specialItem(ID)-- NOT NULL,
-            Type            varchar(128)   NOT NULL,
+            SpecialItem_ID  int   FOREIGN KEY REFERENCES specialItem(ID), -- NOT NULL,
+            [Type]            varchar(128)   NOT NULL,
             BodyPart        varchar(128)   NOT NULL,
             Gender          varchar(128)   ,
-            PRIMARY KEY (SpecialItem_ID)
-            CONSTRAINT FOREIGN KEY (SpecialItem_ID) references [specialItem] (ID)
+            PRIMARY KEY (SpecialItem_ID),
+            FOREIGN KEY (SpecialItem_ID) references [specialItem] (ID)
 )
 
 CREATE TABLE [user] (
@@ -34,125 +57,126 @@ CREATE TABLE [user] (
             Email           NVARCHAR(320)  NOT NULL,
             AccName         varchar(128)    NOT NULL,
             Pass            BINARY(64)      NOT NULL,
-            RealCurrency    DOUBLE(100,3)          NOT NULL DEFAULT 0,
-            GameCurrency    DOUBLE(100,3)          NOT NULL DEFAULT 0,
+            RealCurrency    decimal(38,3)          NOT NULL DEFAULT 0,
+            GameCurrency    decimal(38,3)          NOT NULL DEFAULT 0,
             Stash_ID        bigint        ,
             PRIMARY KEY (UserID)
         )
 
 CREATE TABLE [stash] (
   ID             bigint                 NOT NULL,
-  User_ID        int                    NOT NULL,
+  [User_ID]        int                    NOT NULL,
   NumberOfTabs   int                    NOT NULL DEFAULT 0,
-  CHECK (NumberOfTabs < 65 and NumberOfTabs > 0)
-  PRIMARY KEY (UserID, ID)
-  CONSTRAINT FOREIGN KEY (User_ID) references [user](UserID)
+  CHECK (NumberOfTabs < 65 and NumberOfTabs > 0),
+  PRIMARY KEY ([User_ID], ID),
+  FOREIGN KEY ([User_ID]) references [user](UserID)
 )
+
 
 CREATE TABLE vendorStash(
   ID             bigint                 NOT NULL,
-  User_ID     NVARCHAR(320)          NOT NULL,
+  [User_ID]     int          NOT NULL,
   TypeCode       nvarchar(200)          NOT NULL,
-  PRIMARY KEY [stash](UserID, ID)
+  PRIMARY KEY ([User_ID], ID),
   --CHECK(TypeCode LIKE 'vendor[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]')
-  CONSTRAINT FOREIGN KEY (User_ID,ID) references [stash](UserID, ID)
+  FOREIGN KEY ([User_ID],ID) references [stash]([User_ID], ID)
 )
 
 CREATE TABLE buyerStash(
   ID             bigint                 NOT NULL,
-  User_Email     NVARCHAR(320)          NOT NULL,
+  [User_ID]     int          NOT NULL,
   TypeCode       nvarchar(200)          NOT NULL,
-  PRIMARY KEY [stash](User_Email, ID)
+  PRIMARY KEY ([User_ID], ID),
   --CHECK(TypeCode LIKE 'buyer[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]')
-  CONSTRAINT FOREIGN KEY (User_ID,ID) references [stash](UserID, ID)
+  FOREIGN KEY ([User_ID],ID) references [stash]([User_ID], ID)
 )
 
 CREATE TABLE stashTabs (
-  ID               bigint                 NOT NULL,
-  User_ID          NVARCHAR(320)          NOT NULL,
+  ID             bigint                 NOT NULL,
+  [User_ID]        int                    NOT NULL,
   [Number]         bigint                 NOT NULL,
-  PRIMARY KEY [stash](ID, User_ID, [Number])
-  CONSTRAINT FOREIGN KEY (ID, User_ID ) references [stash](UserID, ID)
+  PRIMARY KEY (ID, [User_ID], [Number]),
+  FOREIGN KEY ([User_ID], ID ) references [stash] ([User_ID] ,ID )
 )
 
 CREATE TABLE normalTab (
   ID               bigint                 NOT NULL,
-  User_ID          NVARCHAR(320)          NOT NULL,
+  [User_ID]     int          NOT NULL,
   [Number]         bigint                 NOT NULL,
   SlotSpace        int                    NOT NULL DEFAULT 60,
-  PRIMARY KEY [stash](ID, User_ID, [Number])
-  CONSTRAINT FOREIGN KEY (ID, User_ID, [Number] ) references [stashTabs](UserID, ID, [Number])
+  PRIMARY KEY (ID, [User_ID], [Number]),
+  FOREIGN KEY (ID, [User_ID], [Number] ) references [stashTabs](ID, [User_ID], [Number])
 )
 
 CREATE TABLE specialTab (
   ID               bigint                 NOT NULL,
-  User_ID          NVARCHAR(320)          NOT NULL,
+  [User_ID]     int          NOT NULL,
   [Number]         bigint                 NOT NULL,
   SlotSpace        int                    NOT NULL DEFAULT 20,
-  PRIMARY KEY [stash](ID, User_ID, [Number])
-  CONSTRAINT FOREIGN KEY (ID, User_ID, [Number] ) references [stashTabs](UserID, ID, [Number])
+  PRIMARY KEY (ID, [User_ID], [Number]),
+  FOREIGN KEY (ID, [User_ID], [Number] ) references [stashTabs](ID, [User_ID], [Number])
 )
 
 CREATE TABLE item (
 
   ID        bigint            NOT NULL,
-  Price     DOUBLE(100,3)     NOT NULL,
+  Price     decimal(38,3)     NOT NULL,
   Name      varchar(128)      NOT NULL,
   isUnique  bit               NOT NULL DEFAULT 0,
   Upgraded  int               NOT NULL DEFAULT 0,
   [Rank]    int               ,
-  PRIMARY KEY  (ID)
+  PRIMARY KEY (ID)
 )
 
 CREATE TABLE weapon(
-  item_ID    bigint            NOT NULL,
-  SpecialAttributes     varchar(128)  ,--Pode ser um composito ?
-  Damage    double(100,2)      NOT NULL,
-  DamageType  varchar(128)     NOT NULL, -- Atributo multivalor ?
-  CriticalChance decimal(5,2) NOT NULL DEFAULT 000.00,
-  CriticalMutiplier decimal(5,2) NOT NULL DEFAULT 000.00,
+  item_ID               bigint            NOT NULL,
+  SpecialAttributes     varchar(128)  ,    --Pode ser um composito ?
+  Damage                decimal(38,2)      NOT NULL,
+  DamageType            varchar(128)     NOT NULL,      -- Atributo multivalor ?
+  CriticalChance        decimal(5,2) NOT NULL DEFAULT 000.00,
+  CriticalMutiplier     decimal(5,2) NOT NULL DEFAULT 000.00,
 
-  PRIMARY KEY  (ID)
-  CONSTRAINT FOREIGN KEY (ID) references [item]( ID )
+  PRIMARY KEY  (item_ID),
+  FOREIGN KEY (item_ID) references [item]( ID ),
   CHECK (CriticalChance <= 100.00 and CriticalChance >= 000.00
         and CriticalMutiplier <= 100.00 and CriticalMutiplier >= 000.00)
 
 )
 
-CREATE TABLE  ranged (
+CREATE TABLE ranged (
   item_ID    bigint            NOT NULL,
-  Range      int               NOT NULL DEFAULT 100,
+  [Range]      int               NOT NULL DEFAULT 100,
   Accuraccy  DECIMAL(5,2)      NOT NULL DEFAULT 65.000,
 
-  CHECK (Accuraccy <= 100.00 and Accuraccy >= 000.00)
-  CHECK (Range < 1000)
-  PRIMARY KEY  (ID)
-  CONSTRAINT FOREIGN KEY (ID) references [weapon]( ID )
+  CHECK (Accuraccy <= 100.00 and Accuraccy >= 000.00),
+  CHECK ([Range] < 1000),
+  PRIMARY KEY  (item_ID),
+  FOREIGN KEY (item_ID) references [weapon]( item_ID ),
 
 )
 
 CREATE TABLE  physical (
   item_ID       bigint         NOT NULL,
-  PiercingRate  double(7,3)    NOT NULL,
-  FireRate      double(7,3)    NOT NULL,
+  PiercingRate  decimal(7,3)    NOT NULL,
+  FireRate      decimal(7,3)    NOT NULL,
 
-  PRIMARY KEY  (ID)
-  CONSTRAINT FOREIGN KEY (ID) references [ranged]( ID )
-  CHECK (PiercingRate > 0)
-  CHECK (FireRate > 0)
+  PRIMARY KEY  (item_ID),
+  FOREIGN KEY (item_ID) references [ranged]( item_ID ),
+  CHECK (PiercingRate > 0),
+  CHECK (FireRate > 0),
 )
 
 
 
-CREATE TABLE  magical (
+CREATE TABLE magical (
   item_ID    bigint                 NOT NULL,
-  CoolDown   DOUBLE(7,2)            NOT NULL,
+  CoolDown   decimal(6,2)            NOT NULL,
   RadiusOfEffectiveness     int     NOT NULL,
 
-  CHECK (Accuraccy <= 100.00 and Accuraccy >= 000.00)
-  CHECK (CoolDown > 0)
-  PRIMARY KEY  (ID)
-  CONSTRAINT FOREIGN KEY (ID) references [ranged]( ID )
+  CHECK (RadiusOfEffectiveness >= 10.00 and RadiusOfEffectiveness <= 1000.00),
+  CHECK (CoolDown > 0),
+  PRIMARY KEY  (item_ID),
+  FOREIGN KEY (item_ID) references [ranged]( item_ID )
 )
 
 
@@ -161,39 +185,36 @@ CREATE TABLE melee(
     AttackSpeed   DECIMAL(4,3)      NOT NULL DEFAULT 0.625,
     HandNum       int               NOT NULL DEFAULT 1,
     MeleeType     varchar(128)      NOT NULL,
-    CHECK (AttackSpeed > 0 and AttackSpeed < 2.500)
-    CHECK (HandNum > 0 and HandNum < 8)
-    PRIMARY KEY  (ID)
-    CONSTRAINT FOREIGN KEY (ID) references [weapon]( ID )
+    CHECK (AttackSpeed > 0 and AttackSpeed < 2.500),
+    CHECK (HandNum > 0 and HandNum < 8),
+    PRIMARY KEY  (Weapon_ID),
+    FOREIGN KEY (Weapon_ID) references [weapon]( item_ID ),
 
 )
-
 
 CREATE TABLE armor (
 
   ID        bigint            NOT NULL,
   SpecialAttributes           varchar(128),
   Defense   DECIMAL(6,3)      NOT NULL,
-  HealthBonus     decimal(100,5),
-
-  CHECK (Defense <= 100)
-  PRIMARY KEY  (ID)
-  CONSTRAINT FOREIGN KEY (ID) references [item]( ID )
-  CHECK (HealthBonus > 0)
+  HealthBonus     decimal(38,5),
+  
+  CHECK (HealthBonus > 0),
+  CHECK (Defense <= 100),
+  PRIMARY KEY  (ID),
+  FOREIGN KEY (ID) references [item]( ID ),
 )
+go
 CREATE TABLE shield (
 
   ID        bigint            NOT NULL,
   Defense   DECIMAL(6,3)      NOT NULL,
   SpecialAbility    varchar(128),
-  PRIMARY KEY  (ID)
-  CONSTRAINT FOREIGN KEY (ID) references [item]( ID )
-  CHECK (Defense <= 100)
+  
+  CHECK (Defense <= 100),
+  PRIMARY KEY  (ID),
+  FOREIGN KEY (ID) references [item]( ID ),
 )
-
-
-
-
 
 
 
